6bdab192c6c3212e284eb65077c824916153bfb7
Susanta <sdeka@bgsu.edu>
2019-12-18 02:36:18 -0500

    new test with zip

--- python-concepts/pretty-python.py
+++ python-concepts/pretty-python.py
@@ -1,3 +1,6 @@
+# Borrowed from Raymond Hettinger's talk
+# https://www.youtube.com/watch?v=OSGv2VnC0go
+
 # Every list is iterable
 
 # simple reverse iteration of for loop
@@ -5,3 +8,14 @@ colors = ['red', 'green', 'blue', 'yellow']
 
 for color in reversed(colors):
     print(color)
+
+# keep track of indices 
+for i, color in enumerate(colors):
+    print(i, '-->', color)
+
+# print multiple lists together
+names = ['Jon', 'Kate', 'Defie']
+foods = ['cheese', 'sauce', 'chips', 'guac']
+
+for name, color, food in zip(names, colors, foods):
+    print(name, ' - ', color, ' - ', food)
